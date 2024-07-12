bdffecf2c3e396bca0d87aa7b87c4f061c0a0f21
WiNkY51 <110772668+WiNkY51@users.noreply.github.com>
2024-07-12 21:12:26 +0300

    Initial Commit

---
+++ HW2.02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.02
+//
+//  Created by Winky51 on 12.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
