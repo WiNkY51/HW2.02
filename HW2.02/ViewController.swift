c822bdfdf37ef90e81c548cc31c94dde6ca49fab
WiNkY51 <110772668+WiNkY51@users.noreply.github.com>
2024-07-13 02:32:05 +0300

    Result homeWork

--- HW2.02/ViewController.swift
+++ HW2.02/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var enabledLight = 0
+    
+    @IBOutlet weak var trafficLightRedView: UIImageView!
+    @IBOutlet weak var trafficLightYellowView: UIImageView!
+    @IBOutlet weak var trafficLightGreenView: UIImageView!
+    @IBOutlet weak var toggleTrafficLightsButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        trafficLightRedView.tintColor = .red
+        trafficLightYellowView.tintColor = .yellow
+        trafficLightGreenView.tintColor = .green
+        
+        trafficLightRedView.alpha = 0.2
+        trafficLightYellowView.alpha = 0.2
+        trafficLightGreenView.alpha = 0.2
+        
+        toggleTrafficLightsButton.setTitle("Включить светофор", for: .normal)
     }
 
-
+    @IBAction func toggleButtonDidTapt(_ sender: UIButton) {
+        sender.setTitle("Переключить светофор", for: .normal)
+        
+        switch enabledLight {
+        case 1:
+            enabledLight = 2
+            trafficLightRedView.alpha = 0.2
+            trafficLightYellowView.alpha = 1
+        case 2:
+            enabledLight = 3
+            trafficLightYellowView.alpha = 0.2
+            trafficLightGreenView.alpha = 1
+        case 3:
+            enabledLight = 1
+            trafficLightGreenView.alpha = 0.2
+            trafficLightRedView.alpha = 1
+        default:
+            enabledLight = 1
+            trafficLightRedView.alpha = 1
+        }
+    }
 }
 
